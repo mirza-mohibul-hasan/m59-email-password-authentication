a76d65982ebf0c2d0e181b473f7797189148bb53
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-04-26 23:12:38 +0600

    developed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import app from '../../firebase/firebase.config';
+import { Link } from 'react-router-dom';
 
+const auth = getAuth(app);
 const Login = () => {
+
+    const [errmsg, setErrmsg] = useState('')
+    const [succmsg, setSuccmsg] = useState('')
+    const emailRef = useRef();
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(email, password)
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result)
+                setErrmsg('')
+                setSuccmsg('Login successfull')
+            })
+            .catch(error => {
+                console.log(error);
+                setSuccmsg('')
+                setErrmsg('Login failed')
+            })
+    }
+    const handleResetPassword = event => {
+        const email = emailRef.current.value;
+        console.log("Your Mail", email);
+        if (!email) {
+            alert('Please enter a email address')
+            return;
+        }
+        sendPasswordResetEmail(auth, email)
+            .then(result => {
+                alert('Please check your email')
+            })
+            .catch(error => {
+                console.log(error)
+                setErrmsg(error.message)
+            })
+    }
+
     return (
         <div>
-            <h2>Login Page</h2>
+            <h2 className='text-center my-2 text-lg font-bold text-blue-700'>Login Here</h2>
+            <h2 className='text-center my-2 text-lg font-bold text-blue-700'>{succmsg}</h2>
+            <h2 className='text-center my-2 text-lg font-bold text-yellow-700'>{errmsg}</h2>
+            <div className='flex justify-center flex-col items-center'>
+                <form onSubmit={handleLogin} className='form flex justify-center flex-col items-center gap-5 text-lg p-2' >
+                    <input ref={emailRef} type="email" name="email" id="email" placeholder='Your Email' required className='border rounded p-3 w-full' />
+
+                    <input type="password" name="password" id="password" placeholder='Your password' required className='border rounded p-3 w-full' />
+
+                    <input type="submit" value="Login" className='border w-[25%] p-3 bg-blue-600 hover:bg-amber-500 px-2 py-1 rounded text-white' />
+                    <p className='flex gap-10'>
+                        <button onClick={handleResetPassword} className='text-blue-700 underline hover:text-amber-800'>Reset Password</button>
+                        <button className='text-blue-700 underline hover:text-amber-800'><Link to='/register'>Register</Link></button>
+                    </p>
+                </form>
+
+            </div>
         </div>
     );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,68 +1,98 @@
 import React, { useState } from 'react';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
 import app from '../../firebase/firebase.config';
+import './Register.css'
+import { Link } from 'react-router-dom';
 
-const auth  = getAuth(app);
+const auth = getAuth(app);
 const Register = () => {
 
     const [email, setEmail] = useState('');
-    const handleEmailOnChange = (event)=>{
-        setEmail(event.target.value);
+    const handleEmailOnChange = (event) => {
+        // setEmail(event.target.value);
         // console.log(email)
     }
     const [password, setPassword] = useState('');
-    const handlePasswordOnBlur = (event) =>{
-        setPassword(event.target.value);
+    const handlePasswordOnBlur = (event) => {
+        // setPassword(event.target.value);
         // console.log(event.target.value)
     }
     const [errormsg, setErrormsg] = useState('');
     const [successmsg, setSuccessmsg] = useState('');
-    const handleSubmit = (event) =>{
+    const handleSubmit = (event) => {
         event.preventDefault();
         setErrormsg('')
         // console.log(event.target.email.value)
         // console.log(event.target.password.value)
         const email = event.target.email.value;
         const password = event.target.password.value;
-        if(!/(?=.*?[A-Z])/.test(password)){
+        const name = event.target.name.value;
+        if (!/(?=.*?[A-Z])/.test(password)) {
             setErrormsg('Add at least one capital letter');
             return;
         }
-        else if(!/(?=.*?[a-z])/.test(password)){
+        else if (!/(?=.*?[a-z])/.test(password)) {
             setErrormsg('Add at least one small letter');
             return;
         }
-        else if(!/(?=.*?[0-9])/.test(password)){
+        else if (!/(?=.*?[0-9])/.test(password)) {
             setErrormsg('Add at least one digit');
             return;
         }
 
         createUserWithEmailAndPassword(auth, email, password)
-        .then(userCredential => {
-            console.log(userCredential);
-            setSuccessmsg('Registration Successfull');
-            setErrormsg('')
-            event.target.reset();
+            .then(userCredential => {
+                console.log(userCredential);
+                setSuccessmsg('Registration Successfull');
+                setErrormsg('')
+                event.target.reset();
+
+                //calling send verification
+                sendVerification(userCredential.user)
+                updateUserData(userCredential.user, name)
+            })
+            .catch(error => {
+                console.error(error.message)
+                setSuccessmsg('')
+                setErrormsg(error.message)
+            })
+    }
+    const sendVerification = user =>{
+        sendEmailVerification(user)
+        .then(result => {
+            console.log(result)
+            alert('Verify Email')
         })
-        .catch(error =>{
-            console.error(error.message)
-            setSuccessmsg('')
+    }
+    const updateUserData = (user, name) =>{
+        updateProfile(user,{
+            displayName: name
+        })
+        .then(result =>{
+            console.log('Update User Name')
+        })
+        .catch(error => {
             setErrormsg(error.message)
         })
     }
-    
+
     return (
         <div className='mt-10'>
             <h2 className='text-center my-2 text-lg font-bold text-red-700'>{errormsg}</h2>
             <h2 className='text-center my-2 text-lg font-bold text-red-700'>{successmsg}</h2>
             <h2 className='text-center my-2 text-lg font-bold text-blue-700'>Regester Here</h2>
-            <form onSubmit={handleSubmit} className='flex justify-center flex-col items-center gap-5 text-lg'>
-                <input onChange={handleEmailOnChange} type="email" name="email" id="email" placeholder='Your Email' required className='border rounded w-[25%] p-3'/>
+            <div className='flex justify-center'>
+                <form onSubmit={handleSubmit} className='form flex justify-center flex-col items-center gap-5 text-lg p-2' >
+                    <input onChange={handleEmailOnChange} type="email" name="email" id="email" placeholder='Your Email' required className='border rounded p-3 w-full'/>
 
-                <input onBlur={handlePasswordOnBlur} type="password" name="password" id="password" placeholder='Your password' required className='border rounded w-[25%] p-3'/>
+                    <input type="text" name="name" id="name" placeholder='Your Name' required className='border rounded  p-3 w-full' />
 
-                <input type="submit" value="Submit" className='border w-[5%] p-3 bg-blue-600 hover:bg-amber-500 px-2 py-1 rounded text-white'/>
-            </form>
+                    <input onBlur={handlePasswordOnBlur} type="password" name="password" id="password" placeholder='Your password' required className='border rounded p-3 w-full' />
+
+                    <input type="submit" value="Register" className='border w-[25%] p-3 bg-blue-600 hover:bg-amber-500 px-2 py-1 rounded text-white' />
+                    <button className='text-blue-700 underline hover:text-amber-800'><Link to='/login'>Login</Link></button>
+                </form>
+            </div>
         </div>
     );
 };
